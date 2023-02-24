245bb4c3f9d37b0df4910104cb66ac0122b9bbb5
IrvingGlt <41974385+Krpeta21@users.noreply.github.com>
2023-02-24 15:44:44 -0600

    select props componente form a componente principal

--- src/components/SummonerComponent/selectFormInput.jsx
+++ src/components/SummonerComponent/selectFormInput.jsx
@@ -1,6 +1,12 @@
-export default function SelectFormInput(){
+export default function SelectFormInput({changeRegion}){
+    
+
+    const handleSelectRegionChange = (event) =>{
+        const newRegion = event.target.value
+        changeRegion(newRegion)
+    }
     return(
-        <select name="summoner" id="summoner">            
+        <select name="summoner" id="summoner" onChange={handleSelectRegionChange}>            
             <option value="NA">NA</option>
             <option value="LAN">LAN</option>
             <option value="EUW">EUW</option>
--- src/components/SummonerComponent/summonerInput.jsx
+++ src/components/SummonerComponent/summonerInput.jsx
@@ -1,23 +1,36 @@
 import SelectFormInput from './selectFormInput';
 import { useState } from 'react';
 import './summonerInput.css'
+
 export default function SummonerInput() {
+    const [summonerName,setSummonerName] = useState('')
+    const [region,setRegion] = useState('NA')
+
+    const handleRegionData = (newRegion) =>{
+      setRegion(newRegion)
+    }
     const handleSubmit = (event) =>{
         event.preventDefault()
         if(!summonerName) return
     }
-    const handleChange =(event)=>{
+    const handleInputChange =(event)=>{
       const summoner = event.target.value
       setSummonerName(summoner)
     }
-    const [summonerName,setSummonerName] = useState('')
+    
+    const handleClearInput= () =>{
+      setSummonerName('')
+    }
+
+
     return (
     <div className='SummonerInput'>
       <img src="/logoLOL.svg" alt="League of Legends LOGO" className='LogoLOL'/>
       <form onSubmit={handleSubmit}>
-        <SelectFormInput/>
-        <input type="text" onChange={handleChange} />
+        <SelectFormInput changeRegion={handleRegionData}/>
+        <input type="text" onChange={handleInputChange} value={summonerName}/>
         {/* <button><span className="button_top">Search</span>!</button> */}
+        <button onClick={handleClearInput}>limpiar</button>
       </form>
     </div>
   );
